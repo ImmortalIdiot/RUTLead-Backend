70cb493867bcf502cb0299277ff4273d97a1261d
Andrey Maltsev <zeeezya@gmail.com>
2024-04-17 19:19:13 +0300

    refactor: add a null check

--- api/Service/TokenService.cs
+++ api/Service/TokenService.cs
@@ -12,18 +12,35 @@ public class TokenService : ITokenService{
     private readonly SymmetricSecurityKey _key;
     public TokenService(IConfiguration config){
         _config  = config;
-        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:SecretKey"]));
+        string? secretKey = _config["JWT:SecretKey"];
+        if (!string.IsNullOrEmpty(secretKey)){
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        }
+        else{
+            throw new InvalidDataException("JWT secret key is missing or empty.");
+        }
+        // _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:SecretKey"]));
     }
 
     public string CreateToken(AppUser user)
     {
         var claims = new List<Claim>
         {
-            new Claim(JwtRegisteredClaimNames.Email, user.Email),
-            new Claim(JwtRegisteredClaimNames.GivenName, user.UserName)
+            new Claim(JwtRegisteredClaimNames.NameId, user.StudentId.ToString())
         };
 
-        var creds = new SigningCredentials(_key,SecurityAlgorithms.HmacSha256Signature);
+        if (!string.IsNullOrEmpty(user.Email))
+        {
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        }
+
+        if (!string.IsNullOrEmpty(user.UserName))
+        {
+            claims.Add(new Claim(JwtRegisteredClaimNames.GivenName, user.UserName));
+        }
+
+
+        var creds = new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
         
         var tokenDescriptor = new SecurityTokenDescriptor
         {
