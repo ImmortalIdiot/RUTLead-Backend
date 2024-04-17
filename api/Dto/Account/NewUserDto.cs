533c251035116a710f6c044e3bb8a3542ad0783a
Andrey Maltsev <zeeezya@gmail.com>
2024-04-17 19:15:57 +0300

    refactor: replace '?' with 'required' in StudentId

--- api/Dto/Account/NewUserDto.cs
+++ api/Dto/Account/NewUserDto.cs
@@ -2,7 +2,9 @@ namespace api.Dto.Account;
 
 public class NewUserDto
 {
-    public string FullName { get; set; }
-    public string Email { get; set; }
-    public string Token { get; set; }
+    public required int StudentId { get; set; }
+    public string? Email { get; set; }
+    public string? FullName { get; set; }
+    public string? Group { get; set; }
+    public required string Token { get; set; }
 }
--- api/Models/AppUser.cs
+++ api/Models/AppUser.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 namespace api.Models{
     public class AppUser : IdentityUser
     {
-        
+        public required int StudentId { get; set; }
+        public required string Group { get; set; }
     }
 }
