57adf5ed5d878157e3f1aa29f911e7f10c9b29fe
Andrey Maltsev <zeeezya@gmail.com>
2024-04-21 21:14:57 +0300

    refactor:  replace '?' with 'required' in 'Email', 'FullName' and 'Group'

--- api/Dto/Account/NewUserDto.cs
+++ api/Dto/Account/NewUserDto.cs
@@ -3,8 +3,8 @@ namespace api.Dto.Account;
 public class NewUserDto
 {
     public required int StudentId { get; set; }
-    public string? Email { get; set; }
-    public string? FullName { get; set; }
-    public string? Group { get; set; }
+    public required string Email { get; set; }
+    public required string FullName { get; set; }
+    public required string Group { get; set; }
     public required string Token { get; set; }
 }
