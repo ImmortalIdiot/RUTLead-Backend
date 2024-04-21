e2d3fc5e789191c232f0ef20a1a7dc661d2bfdd0
Andrey Maltsev <zeeezya@gmail.com>
2024-04-21 21:30:29 +0300

    style: format code; chore: add the exception checking; refactor: the SecretKey is taken from environment variables, not from appseting.json

--- api/Service/TokenService.cs
+++ api/Service/TokenService.cs
@@ -10,22 +10,17 @@ namespace api.Service;
 public class TokenService : ITokenService{
     private readonly IConfiguration _config;
     private readonly SymmetricSecurityKey _key;
+
     public TokenService(IConfiguration config){
         _config  = config;
-        string? secretKey = _config["JWT:SecretKey"];
-        if (!string.IsNullOrEmpty(secretKey)){
-            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
-        }
-        else{
-            throw new InvalidDataException("JWT secret key is missing or empty.");
-        }
-        
+
+        string secretKey = Environment.GetEnvironmentVariable("SECRET_KEY");
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
     }
 
     public string CreateToken(AppUser user)
     {
-        var claims = new List<Claim>
-        {
+        var claims = new List<Claim> {
             new Claim(JwtRegisteredClaimNames.NameId, user.StudentId.ToString())
         };
 
@@ -33,24 +28,28 @@ public class TokenService : ITokenService{
         {
             claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
             claims.Add(new Claim(JwtRegisteredClaimNames.GivenName, user.UserName));
+        } else {
+            throw new InvalidDataException("Email or UserName is missing or empty.");
         }
 
+        try {
+            var creds = new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
+            
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = creds,
+                Issuer = _config["JWT:Issuer"],
+                Audience = _config["JWT:Audience"]
+            };
 
-        var creds = new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
-        
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddDays(7),
-            SigningCredentials = creds,
-            Issuer = _config["JWT:Issuer"],
-            Audience = _config["JWT:Audience"]
-        };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            var token = tokenHandler.CreateToken(tokenDescriptor);
 
-        var tokenHandler = new JwtSecurityTokenHandler();
-
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        
-        return tokenHandler.WriteToken(token);
+            return tokenHandler.WriteToken(token);
+        } catch {
+            throw new Exception("Failed to create token.");
+        }
     }
 }
