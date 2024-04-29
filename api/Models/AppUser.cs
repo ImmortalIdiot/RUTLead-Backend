5461fa2564c15e43f4ac901332ca6374fae721d3
Andrey Maltsev <zeeezya@gmail.com>
2024-04-29 19:15:31 +0300

    delete: delete the AppUser class

--- api/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace api.Models {
-    public class AppUser : IdentityUser
-    {
-        public required int StudentId { get; set; }
-        public required string Group { get; set; }
-    }
-}
