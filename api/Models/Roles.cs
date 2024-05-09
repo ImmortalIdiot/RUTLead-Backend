6512489fc7c81783a4508903a825c4372a89afb9
Andrey Maltsev <zeeezya@gmail.com>
2024-05-09 20:29:22 +0300

    feature: create a static class 'Roles'

---
+++ api/Models/Roles.cs
@@ -0,0 +1,7 @@
+namespace api.Models {
+    public static class Roles {
+        public static readonly string Student = "Student";
+        public static readonly string Monitor = "Monitor";
+        public static readonly string Deanery = "Deanery";
+    }
+}
